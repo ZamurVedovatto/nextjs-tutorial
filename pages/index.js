191d57ed8945ee227835194bf714fcd004ea46a8
Zamur Vedovatto <vedovatto@gmail.com>
2021-06-03 17:59:31 -0300

    linking pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
@@ -9,6 +10,9 @@ export default function Home() {
     <div>
       <Navbar />
       <h1>homepage</h1>
+      <Link href="/ninjas">
+        <a>See Listing</a>
+      </Link>
       <Footer />
     </div>
   )
